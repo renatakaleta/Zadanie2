4bbf66f72e8f50fd0f4234f4eb0b7173b89ca67d
Renata <Reniakk95@gmail.com>
2023-04-19 09:38:35 +0200

    zad2-complete

--- src/main/java/Pages/ProductParametsPage.java
+++ src/main/java/Pages/ProductParametsPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v85.systeminfo.model.Size;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
